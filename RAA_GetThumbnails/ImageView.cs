2d57dd8fe05bcbe4dd53106609dd9baca7cf3f6f
kilkellym <michael@archsmarter.com>
2024-03-11 11:49:49 -0400

    Add project files.

---
+++ RAA_GetThumbnails/ImageView.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace RAA_GetThumbnails
+{
+	internal class ImageView
+	{
+		[DllImport("gdi32.dll")]
+		public static extern bool DeleteObject(IntPtr hObject);
+		public static List<ImageEntity> GetAllImagesData(List<string> listFamilyFiles)
+		{
+			try
+			{
+				// return images data
+				//int THUMB_SIZE = 256;
+				int THUMB_SIZE = 200;
+				List<ImageEntity> list = new List<ImageEntity>();
+				foreach (string familyFile in listFamilyFiles)
+				{
+					ImageEntity ie = new ImageEntity();
+					ie.ImagePath = familyFile;
+					ie.FileName = Path.GetFileNameWithoutExtension(familyFile);
+
+					//Bitmap thumbnail = WindowsThumbnailProvider.GetThumbnail(familyFile, THUMB_SIZE, THUMB_SIZE, ThumbnailOptions.None);
+
+					using (System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(THUMB_SIZE, THUMB_SIZE))
+					{
+						IntPtr hBitmap = WindowsThumbnailProvider.GetThumbnail(familyFile, THUMB_SIZE, THUMB_SIZE, ThumbnailOptions.None);
+
+						try
+						{
+							ie.ImageBitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+								hBitmap, IntPtr.Zero, System.Windows.Int32Rect.Empty, System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+						}
+						finally
+						{
+							DeleteObject(hBitmap);
+						}
+					}
+
+					list.Add(ie);
+				}
+				return list;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+	}
+	public class ImageEntity
+	{
+		public string ImagePath { get; set; }
+		public string FileName { get; set; }
+		public System.Windows.Media.ImageSource ImageBitmap { get; set; }
+	}
+}
---
+++ RAA_GetThumbnails/ImageWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RAA_GetThumbnails
+{
+	/// <summary>
+	/// Interaction logic for ImageWindow.xaml
+	/// </summary>
+	public partial class ImageWindow : Window
+	{
+		public ObservableCollection<ImageEntity> Images { get; set; }
+		public ImageWindow(List<ImageEntity> imageList)
+		{
+			InitializeComponent();
+			Images = new ObservableCollection<ImageEntity>();
+			this.DataContext = this;
+			LoadImages(imageList);
+		}
+
+		// Method to add images, call this method to populate your grid
+		public void LoadImages(IEnumerable<ImageEntity> imageEntities)
+		{
+			foreach (var imageEntity in imageEntities)
+			{
+				Images.Add(imageEntity);
+			}
+
+			ImageGrid.ItemsSource = Images;
+		}
+
+		// Optionally, you can load images from paths directly (example method)
+		public void AddImageFromPath(string path, string fileName)
+		{
+			var imageEntity = new ImageEntity
+			{
+				ImagePath = path,
+				FileName = fileName,
+				ImageBitmap = new BitmapImage(new Uri(path))
+			};
+
+			Images.Add(imageEntity);
+		}
+
+		private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			TaskDialog td = new TaskDialog("Image Clicked");
+			
+			TaskDialog.Show("Image Clicked", "Insert family into Revit");
+		}
+	}
+}
